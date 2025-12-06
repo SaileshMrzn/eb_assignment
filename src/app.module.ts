65787b335ca08eeff955b00a34274c1557864e8c
SaileshMrzn <shailesh.mrzn@gmail.com>
2025-12-06 08:34:23 +0545

    project init and database connection setup

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'class',
+      },
+    }),
+
+    MongooseModule.forRoot(
+      process.env.MONGO_URI ?? 'mongodb://localhost:27017/',
+    ),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
